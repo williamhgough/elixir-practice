a7cf6e628ced948f8748f4905caed666aeacfbe0
William Gough <williamhenrygough@gmail.com>
2016-11-21 22:43:51 +0000

    Implement Topic addition & success redirect

---
+++ discuss/web/static/css/app.js
@@ -0,0 +1,34 @@
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
+
+export default StyleSheet.create({
+    "body": {
+        "fontFamily": "'Open Sans', sans-serif",
+        "marginTop": 0
+    },
+    "h1": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "h2": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "h3": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "h4": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "h5": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "h6": {
+        "fontFamily": "'Open Sans', sans-serif"
+    },
+    "nav ul a": {
+        "fontSize": 1.5
+    }
+});
